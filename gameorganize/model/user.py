f0d53b6446fbdfee7823bdb86419fab1c28ed91a
Chris <cjriddle31@gmail.com>
2025-07-29 13:47:13 -0700

    Reduce password hashing algo heaviness

--- gameorganize/model/user.py
+++ gameorganize/model/user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 from .game import GameEntry
 from .platform import Platform
 import re
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 rex_password = re.compile("^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$")
 
@@ -14,6 +14,9 @@ class User(UserMixin, db.Model):
     password_hash: Mapped[int] = mapped_column()
     games = relationship('GameEntry', backref='user')
     platforms = relationship('Platform', backref='user')
+
+    def hash_password(self, password):
+        return generate_password_hash(password, method='pbkdf2:sha1:300')
 
     @validates('username')
     def validate_username(self,key,value):
@@ -29,4 +32,7 @@ class User(UserMixin, db.Model):
     def password(self, password):
         if(not rex_password.match(password)):
             raise ValueError("Password must be at least 8 characters long and contain an uppercase letter")
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha1')
+        self.password_hash = self.hash_password(password)
+
+    def check_password_matches(self, password):
+        return check_password_hash(self.password_hash, password)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,14 @@
 from gameorganize.model.user import User
 import pytest
 
+def test_pass_hash(db_session):
+    user1 = User(
+        username="samename",
+        password="GoodPassword",
+    )
+
+    assert(user1.check_password_matches("GoodPassword"))
+
 def test_dupe_user(db_session):
     user1 = User(
         username="samename",
@@ -18,16 +26,19 @@ def test_dupe_user(db_session):
         db_session.commit()
     db_session.rollback()
 
+def test_lotta_users(db_session):
+    for i in range(0, 100):
+        new_user = User(
+            username=f"samename{i}",
+            password="GoodPassword",
+        )
+        db_session.add(new_user)
+    db_session.commit()
+    db_session.rollback()
+
 def test_bad_password(db_session):
     with pytest.raises(Exception) as e:
         bad_user = User(
             username="badbaduser",
             password="pass",
-        )
-
-def test_bad_user(db_session):
-    with pytest.raises(Exception) as e:
-        bad_user = User(
-            username="",
-            password="GoodPassword",
         )
